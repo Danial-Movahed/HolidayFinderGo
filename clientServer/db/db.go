52d25c498ee4bebbf1ce73c7f65bdee2ba4cbbd5
Amir <ah.gharaghani@gmail.com>
2023-06-28 00:53:25 +0330

    fixed a few problems

--- clientServer/db/db.go
+++ clientServer/db/db.go
@@ -12,16 +12,8 @@ type DB struct {
 	connection *sql.DB
 }
 
-type noHolidaysError struct {
-	Date string
-}
-
-func (e *noHolidaysError) Error() string {
-	return fmt.Sprintf("No holidays on %s", e.Date)
-}
-
 func (db *DB) Connect() error {
-	connStr := fmt.Sprintf("host=%s:%v dbname=%s user=%s password=%s sslmode=disable", host, port, user, password, dbname)
+	connStr := fmt.Sprintf("host=%s:%v dbname=%s user=%s password=%s sslmode=require", host, port, user, password, dbname)
 	connection, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return err
@@ -40,36 +32,24 @@ func (db *DB) Close() error {
 	return db.connection.Close()
 }
 
-func (db *DB) Find(req *grpc.HolidayRequest) (grpc.Holiday, error) {
+func (db *DB) GetHoliday(req *grpc.HolidayRequest) (grpc.Holiday, error) {
 	date := fmt.Sprintf("%s-%s-%s", req.Year, req.Month, req.Day)
 
 	selectionQuery := "SELECT FROM Holidays WHERE date = '$1'"
 	rows, err := db.connection.Query(selectionQuery, date)
 
 	if err != nil {
-		return grpc.Holiday{
-			Name:        "",
-			Description: "",
-		}, err
+		return grpc.Holiday{}, err
 	}
-
 	defer rows.Close()
 
 	if rows.Next() {
 		var name, description string
 		if err := rows.Scan(&name, &description); err != nil {
-			return grpc.Holiday{
-				Name:        "",
-				Description: "",
-			}, err
+			return grpc.Holiday{}, err
 		}
 		if name == "" && description == "" {
-			return grpc.Holiday{
-					Name:        name,
-					Description: description,
-				}, &noHolidaysError{
-					Date: date,
-				}
+			return grpc.Holiday{}, fmt.Errorf("no holidays on %s", date)
 		}
 		return grpc.Holiday{
 			Name:        name,
@@ -79,14 +59,11 @@ func (db *DB) Find(req *grpc.HolidayRequest) (grpc.Holiday, error) {
 	} else {
 		// If no holiday is found reports to client and asks for new information
 		// Then saves to Database and returns results to gRtc server
-		return grpc.Holiday{
-			Name:        "",
-			Description: "",
-		}, err
+		return grpc.Holiday{}, err
 	}
 
 }
 
-// func (db *DB) Add() {
+// func (db *DB) registerHoliday() {
 
 // }
